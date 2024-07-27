41ee386fec9fd796b40517075c7500841c07bf00
venkattech10 <venkateswar.tech7@gmail.com>
2024-07-27 15:48:00 +0530

    Left Triangle Pattern program added

---
+++ src/com/venkat/java/general/LeftTrianglePatternDemo.java
@@ -0,0 +1,23 @@
+package com.venkat.java.general;
+
+public class LeftTrianglePatternDemo {
+    public static void main(String[] args) {
+        int rows = 5;
+        //Note : If we want to print 1 2 3's we have to take variable & increment based on row value
+        //Outer loop to print given rows
+        for (int i = 0; i < rows; i++) {
+            //Inner loop to print the spaces to those many spaces * 2
+            //If we comment this for loop this program works like RightTrianglePattern
+            for (int j = 2 * (rows - i); j >= 0; j--) {
+                System.out.print(" ");
+            }
+            //Inner loop to print columns for each row
+            for (int j = 0; j <= i; j++) {
+                //Print * & space
+                System.out.print("* ");
+            }
+            //To move cursor to next line
+            System.out.println();
+        }
+    }
+}
