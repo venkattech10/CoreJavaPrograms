e72685622b28c045fbc21cb342ac3cbc7379f1fc
venkattech10 <venkateswar.tech7@gmail.com>
2024-07-27 16:10:47 +0530

    String reverse program modified

--- src/com/venkat/java/strings/StringReverseDemo.java
+++ src/com/venkat/java/strings/StringReverseDemo.java
@@ -3,9 +3,9 @@ package com.venkat.java.strings;
 public class StringReverseDemo {
     public static void main(String[] args) {
         String input = "Hello World";
-        System.out.println("********* Approach -1 **********");
+        System.out.println("********* Approach - 1 **********");
         //Using StringBuffer
         StringBuffer stringBuffer = new StringBuffer(input);
-        System.out.println(stringBuffer.reverse());
+        System.out.println(stringBuffer.reverse()); // dlroW olleH
     }
 }
